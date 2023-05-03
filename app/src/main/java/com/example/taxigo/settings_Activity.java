febbc142ad161368a3cc3673c06eb1ce7d845f3d
guddu <guddulawaniya123@gmail.com>
2023-05-04 02:05:19 +0530

    03-05-2023

--- app/src/main/java/com/example/taxigo/settings_Activity.java
+++ app/src/main/java/com/example/taxigo/settings_Activity.java
@@ -1,6 +1,11 @@
 package com.example.taxigo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +15,40 @@ public class settings_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        LinearLayout profile = findViewById(R.id.profilelinear);
+        LinearLayout favoritelinear = findViewById(R.id.favoritelinear);
+        LinearLayout preferencelinear = findViewById(R.id.preferencelinear);
+        LinearLayout languagelinear = findViewById(R.id.languagelinear);
+        LinearLayout aboutlinear = findViewById(R.id.aboutlinear);
+        LinearLayout betalinear = findViewById(R.id.betalinear);
+        LinearLayout logoutlinear = findViewById(R.id.logoutlinear);
+
+        TextView support = findViewById(R.id.supportsetting);
+
+        support.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Redirect_support_Activity.class));
+            }
+        });
+
+        ImageView backbutton = findViewById(R.id.settingbackbutton);
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), profile_Activity.class));
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/taxigo/support_Activity.java
+++ app/src/main/java/com/example/taxigo/support_Activity.java
@@ -1,6 +1,10 @@
 package com.example.taxigo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,20 @@ public class support_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_support);
+
+        ImageView supportback = findViewById(R.id.supportbackbutton);
+        TextView viewallride = findViewById(R.id.viewallride);
+        viewallride.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),select_an_order_Activity.class));
+            }
+        });
+        supportback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
